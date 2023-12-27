ca3fba74880436cc3c51d28703d2fc83842968c2
renyakun <13843076014@163.com>
2023-12-27 13:43:31 +0800

    Initial Commit

---
+++ WhyNotTry/WhyNotTryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhyNotTryApp.swift
+//  WhyNotTry
+//
+//  Created by 任亚坤 on 2023/12/27.
+//
+
+import SwiftUI
+
+@main
+struct WhyNotTryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
